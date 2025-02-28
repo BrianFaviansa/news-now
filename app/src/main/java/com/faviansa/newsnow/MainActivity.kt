5c42ebd8e30932a2a21bff774e01aa094ce9897d
BrianFaviansa <brianfpd31@gmail.com>
2025-02-28 09:30:22 +0700

    feat(ui): integrate splash screen for improved app launch experience

--- app/src/main/java/com/faviansa/newsnow/MainActivity.kt
+++ app/src/main/java/com/faviansa/newsnow/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.compose.rememberNavController
 import com.faviansa.newsnow.ui.navigation.AppNavigation
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,6 +24,8 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
 
         setContent {
             MaterialTheme {
