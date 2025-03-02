3419dfeb69463a1ad49f276ebfafbadf570c8794
BrianFaviansa <brianfpd31@gmail.com>
2025-03-02 10:41:25 +0700

    feat(ui): update SearchBar layout to fill width and adjust padding for better appearance

--- app/src/main/java/com/faviansa/newsnow/ui/search/SearchScreen.kt
+++ app/src/main/java/com/faviansa/newsnow/ui/search/SearchScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -103,7 +104,8 @@ fun SearchScreen(
         ) {
             SearchBar(
                 modifier = Modifier
-                    .padding(vertical = 10.dp)
+                    .fillMaxWidth()
+                    .padding(vertical = 10.dp, horizontal = 4.dp)
                     .focusRequester(focusRequester)
                     .onFocusChanged { isSuggestionChipsVisible = it.isFocused },
                 query = searchQuery,
